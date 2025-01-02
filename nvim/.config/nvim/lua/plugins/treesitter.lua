e29bdfd35a22e910d0bd1461050548c8dd4c3d4c
velsin <16018965+velsin@users.noreply.github.com>
2025-01-02 16:53:54 +0100

    Update neovim config.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,34 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
+    build = ":TSUpdate",
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = false,
+
+	ignore_install = { },
+
+	highlight = {
+	  enable = true,
+	  disable = function(lang, buf)
+	    local max_filesize = 100 * 1024 -- 100 KB
+	    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+	    if ok and stats and stats.size > max_filesize then
+	      return true
+	    end
+	  end,
+
+	  additional_vim_regex_highlighting = true,
+	},
+      }
+    end
   }
 }
