21f1a35b0f1a9ade6e3ecd47b7dd8d0b71582fd3
velsin <16018965+velsin@users.noreply.github.com>
2025-01-07 22:09:06 +0100

    Add multigrep picker

---
+++ nvim/.config/nvim/lua/config/telescope/multigrep.lua
@@ -0,0 +1,57 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local make_entry = require("telescope.make_entry")
+local conf = require("telescope.config").values
+
+local M = {}
+
+local live_multigrep = function(opts)
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.uv.cwd()
+
+  local finder = finders.new_async_job {
+    command_generator = function(prompt)
+      if not prompt or prompt == "" then
+        return nil
+      end
+
+      local pieces = vim.split(prompt, "  ")
+      local args = { "rg" }
+
+      -- The first part of the prompt is the regex pattern
+      if pieces[1] then
+        table.insert(args, "-e")
+        table.insert(args, pieces[1])
+      end
+
+      -- The second part is the glob pattern
+      if pieces[2] then
+        table.insert(args, "-g")
+        table.insert(args, pieces[2])
+      end
+
+      return vim.iter({
+        args,
+        -- Add default config options to the command
+        { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" }
+      }):flatten():totable()
+    end,
+    entry_maker = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  }
+
+  pickers.new(opts, {
+    debounce = 100,
+    prompt_title = "Multigrep",
+    finder = finder,
+    previewer = conf.grep_previewer(opts),
+    sorter = require("telescope.sorters").empty(),
+    theme = "dropdown",
+  }):find()
+end
+
+M.setup = function()
+  vim.keymap.set("n", "<leader>fg", live_multigrep)
+end
+
+return M
