eed82ec4436f3cd3cc3f300330b61ee4b8e6372c
velsin <16018965+velsin@users.noreply.github.com>
2025-01-09 22:03:10 +0100

    Add nvim autocomplete. Add terraform lsp, and attempt to add binary.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,18 @@
 return { {
   "neovim/nvim-lspconfig",
   dependencies = {
-    "folke/lazydev.nvim",
-    ft = "lua",   -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    'saghen/blink.cmp',
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
       },
-    },
+    }
   },
   config = function()
     require("lspconfig").lua_ls.setup {}
@@ -20,6 +23,9 @@ return { {
         }
       }
     }
+    require("lspconfig").terraformls.setup {}
+
+    -- Set up autoformat on save
     vim.api.nvim_create_autocmd('LspAttach', {
       callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
