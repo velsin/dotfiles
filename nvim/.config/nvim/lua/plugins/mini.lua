1218b3760ecedc1be7dd7ad3e94dc5e143a0e3e1
velsin <16018965+velsin@users.noreply.github.com>
2026-02-20 13:38:02 +0100

    nvim: Add mini.surround and mini.ai plugins.

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -4,6 +4,9 @@ return {
     config = function()
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = true }
+
+      require('mini.surround').setup()
+      require('mini.ai').setup()
     end
   }
 }
